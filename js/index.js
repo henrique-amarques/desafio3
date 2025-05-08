36f23a1e4cfd84c56ff4f8545bc46cd9a3c38b44
Henrique Araujo <henrique.marks17@gmail.com>
2025-05-08 15:44:51 -0300

    alteração da variavel i

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const inputPreenchido = document.querySelectorAll('.input')
 const caixa = document.querySelectorAll('.caixa')
 const botaoEnviar = document.getElementsByClassName('btn')[0]
-let i = 0
+
 
 botaoEnviar.addEventListener('click', () =>{
+    let i = 0
     inputPreenchido.forEach(element => {
         if(element.value == ''){
             caixa[i].classList.add('falta')
@@ -11,11 +12,9 @@ botaoEnviar.addEventListener('click', () =>{
             
         }if(element.value !=''){
             caixa[i].classList.remove('falta');
-            element.classList.add('preenchido')
-            
+            element.classList.add('preenchido')   
         }
+
         i= i+1
     });
-i=0
-
 })
